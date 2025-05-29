53670d3fc2066fc35c0de8f686409a99717e2cde
Neal Terrell <nealdt@gmail.com>
2025-05-28 18:19:51 -0700

    Upgrade to SFML 3.0.0

--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,17 @@ void drawPixel(sf::RenderWindow& window, sf::Vector2i position, sf::Color color)
 	std::array<sf::Vertex, 1> pixel = {
 		sf::Vertex(sf::Vector2f(position.x, position.y), color)
 	};
-	window.draw(pixel.data(), 1, sf::Points);
+	window.draw(pixel.data(), 1, sf::PrimitiveType::Points);
 }
 
 int main() {
-	sf::RenderWindow window{ sf::VideoMode{800, 800}, "SFML Demo" };
+	sf::RenderWindow window{ sf::VideoMode::getFullscreenModes().at(0), "SFML Demo"};
 	sf::Clock c;
 	auto last = c.getElapsedTime();
 
 	while (window.isOpen()) {
-		sf::Event ev;
-		while (window.pollEvent(ev)) {
-			if (ev.type == sf::Event::Closed) {
+		while (const std::optional event = window.pollEvent()) {
+			if (event->is<sf::Event::Closed>()) {
 				window.close();
 			}
 		}
